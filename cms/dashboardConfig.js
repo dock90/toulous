cff9f4c397117719075ec560fdad4712152debdd
EdwardGoomba <edward@dock90.io>
2021-01-09 23:59:05 -0700

    Add netlify deploy to sanity

---
+++ cms/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: 'netlify',
+      options: {
+        title: 'Toulous Deploys',
+        sites: [
+          {
+            title: 'Website',
+            apiId: 'a31f1d86-8f39-4236-82f3-95b8bca0f97b',
+            buildHookId: '5ffaa5042b73fca1089161b4',
+            name: 'toulous',
+          },
+        ]
+      }
+    }
+  ]
+}
